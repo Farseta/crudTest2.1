94bb813fc40847a9b83b2ade02110122339b82db
Farseta <109111149+Farseta@users.noreply.github.com>
2024-03-06 11:39:45 +0700

    adding interface in home, total transportation that lending and return plus total aset. adding grafik to interace home to

--- assetRecording/app/Http/Controllers/HomeController.php
+++ assetRecording/app/Http/Controllers/HomeController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use App\Models\Other_asset;
+use App\Models\Transportation;
+use App\Models\Vehicle_lending;
+use App\Models\Vehicle_return;
 
 class HomeController extends Controller
 {
@@ -23,6 +28,34 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $transportation_total = Transportation::count();
+        $transportation_ready = Transportation::where('status', 'ready')->count();
+        $transportation_unready = Transportation::where('status', 'unready')->count();
+        $other_asset_total = Other_asset::count();
+
+        $label_bar = ['Peminjaman','pengembalian'];
+        $data_bar = [];
+        foreach ($label_bar as $key => $value) {
+            $data_bar[$key]['label'] = $label_bar[$key];
+            $data_bar[$key]['backgroundColor'] = $key==1 ? 'rgba(245,40,145,0.8)': 'rgba(40,145,245,0.8)';
+            $data_month = [];
+            foreach (range(1, 12) as $month) {
+                if($key ==0){
+                    $data_month []= Vehicle_lending::select(DB::raw("COUNT(*) as total"))
+                    ->whereMonth('created_at', $month)
+                    ->first()->total;
+                }else{
+                    $data_month []= Vehicle_return::select(DB::raw("COUNT(*) as total"))
+                    ->whereMonth('created_at', $month)
+                    ->first()->total;
+                }
+                
+                    
+            }
+            
+            $data_bar[$key]['data'] = $data_month;
+        }
+        // return $data_bar;
+        return view('home', compact('transportation_total', 'other_asset_total', 'transportation_ready', 'transportation_unready','data_bar'));
     }
 }
--- assetRecording/resources/views/home.blade.php
+++ assetRecording/resources/views/home.blade.php
@@ -1,23 +1,155 @@
 @extends('layouts.admin')
 
+
+@section('CSS')
+@endsection
+
+@section('title')
+    Home
+@endsection
+
+@section('sideTitle')
+    {{ url('home') }}
+@endsection
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div id="controller">
+        <!-- Small boxes (Stat box) -->
+        <div class="row">
+            <div class="col-lg-3 col-12">
+              <!-- small box -->
+              <div class="small-box bg-info">
+                <div class="inner">
+                  <h3>
+                    {{("$transportation_total")}}
+                  </h3>
+  
+                  <p>Total transportasi dimiliki</p>
+                </div>
+                <div class="icon">
+                  <i class="ion ion-bag"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              </div>
+            </div>
+            <!-- ./col -->
+            <div class="col-lg-3 col-12">
+              <!-- small box -->
+              <div class="small-box bg-success">
+                <div class="inner">
+                  <h3>
+                    {{("$transportation_ready")}}
+                  </h3>
+  
+                  <p>Transportasi siap dipakai</p>
+                </div>
+                <div class="icon">
+                  <i class="ion ion-stats-bars"></i>
+                </div>
+                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              </div>
+            </div>
+            <!-- ./col -->
+            <div class="col-lg-3 col-12">
+                <!-- small box -->
+                <div class="small-box bg-danger">
+                  <div class="inner">
+                    <h3>
+                      {{("$transportation_unready")}}
+                    </h3>
+    
+                    <p>Transportasi sedang dipakai</p>
+                  </div>
+                  <div class="icon">
+                    <i class="ion ion-stats-bars"></i>
+                  </div>
+                  <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+              </div>
+              <div class="col-lg-3 col-12">
+                <!-- small box -->
+                <div class="small-box bg-info">
+                  <div class="inner">
+                    <h3>
+                      {{("$other_asset_total")}}
+                    </h3>
+    
+                    <p>Total Asset</p>
+                  </div>
+                  <div class="icon">
+                    <i class="ion ion-bag"></i>
+                  </div>
+                  <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+              </div>
+            <!-- ./col -->
+          </div>
+        <!-- BAR CHART -->
+        <div class="card card-success">
+            <div class="card-header">
+                <h3 class="card-title">Grafik Peminjaman Dan Pengembalian</h3>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                        <i class="fas fa-minus"></i>
+                    </button>
+                    <button type="button" class="btn btn-tool" data-card-widget="remove">
+                        <i class="fas fa-times"></i>
+                    </button>
                 </div>
             </div>
+            <div class="card-body">
+                <div class="chart">
+                    <canvas id="barChart"
+                        style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                </div>
+            </div>
+            <!-- /.card-body -->
         </div>
+        <!-- /.card -->
     </div>
-</div>
+@endsection
+
+@section('JS')
+<!-- jQuery -->
+<script src={{ asset("assets/plugins/jquery/jquery.min.js")}}></script>
+<!-- Bootstrap 4 -->
+
+<script src={{ asset("plugins/bootstrap/js/bootstrap.bundle.min.js")}}></script>
+ <!-- ChartJS -->
+<script src={{ asset("assets/plugins/chart.js/Chart.min.js")}}></script>
+
+
+    <script type="text/javascript">
+    var data_bar = '{!! json_encode($data_bar)!!}';
+    
+
+    var areaChartData = {
+      labels  : ['January', 'February', 'March', 'April', 'May', 'June', 'July','Agustus','September','October','November','December'],
+      datasets: JSON.parse(data_bar),
+    }
+
+    
+        //-------------
+        //- BAR CHART -
+        //-------------
+        var barChartCanvas = $('#barChart').get(0).getContext('2d')
+        var barChartData = $.extend(true, {}, areaChartData)
+        // var temp0 = areaChartData.datasets[0]
+        // var temp1 = areaChartData.datasets[1]
+        // barChartData.datasets[0] = temp1
+        // barChartData.datasets[1] = temp0
+
+        var barChartOptions = {
+            responsive: true,
+            maintainAspectRatio: false,
+            datasetFill: false
+        }
+
+        new Chart(barChartCanvas, {
+            type: 'bar',
+            data: barChartData,
+            options: barChartOptions
+        })
+    </script>
 @endsection
